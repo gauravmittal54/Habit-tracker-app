d3686598d74747b9f4e86467ec6be7b05687d28c
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-10-23 20:57:11 +0530

    Update db_connection.js

--- connection/db_connection.js
+++ connection/db_connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/habbitDB'/*, {useNewUrlParser: true}*/);
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}*/);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
