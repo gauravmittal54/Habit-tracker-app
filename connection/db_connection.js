8d9a57484adc8a66adc1d32c28a893a1ac2f8ff8
gauravmittal54 <61792468+gauravmittal54@users.noreply.github.com>
2023-10-24 10:16:11 +0530

    Update db_connection.js

--- connection/db_connection.js
+++ connection/db_connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}*/);
+mongoose.connect("mongodb://mongo:eTxTs2Gco5NuuyGTQkJ0@containers-us-west-186.railway.app:6104", {useNewUrlParser: true}*/);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
